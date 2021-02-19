a7532eb1746dfe0768bd4d42135733c47e51b43a
Ecxus <Ecxus@gmail.com>
2021-02-19 12:36:27 -0300

    Demonstrações em Map

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,6 +16,18 @@ public class Program {
         //removendo  pela chave email
         cookies.remove("email");
 
+        //No Map contem?
+        System.out.println("No Map contem a chave telefone? "+ cookies.containsKey("telefone"));
+
+        //pegando o valor que contem na chave...
+        System.out.println("O numero do telefone é: "+cookies.get("telefone"));
+
+        //buscanco com map um valor que não existe
+        System.out.println("Email: "+cookies.get("email"));
+
+        //tamanho do Map
+        System.out.println("Tamanho do Map: "+cookies.size());
+
         System.out.println("Lista de todos: ");
         for(String key : cookies.keySet()){
             System.out.println(key +": "+cookies.get(key));
